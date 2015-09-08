03b7a70338b41ef8dea327ebaa77faf47ca30a34
kw <waeschle@ub.uni-heidelberg.de>
2015-09-08 13:10:46 +0200

    Functioning version with all DC elements present in XMetaDissPlus

--- filter/Xmdp22SchemaPublicationFormatAdapter.inc.php
+++ filter/Xmdp22SchemaPublicationFormatAdapter.inc.php
@@ -44,11 +44,11 @@ class Xmdp22SchemaPublicationFormatAdapter extends MetadataDataObjectAdapter {
 	//
 	/**
 	 * @see MetadataDataObjectAdapter::injectMetadataIntoDataObject()
-	 * @param $dc11Description MetadataDescription
+	 * @param $description MetadataDescription
 	 * @param $publicationFormat PublicationFormat
 	 * @param $authorClassName string the application specific author class name
 	 */
-	function &injectMetadataIntoDataObject(&$dc11Description, &$publicationFormat, $authorClassName) {
+	function &injectMetadataIntoDataObject(&$description, &$publicationFormat, $authorClassName) {
 		// Not implemented
 		assert(false);
 	}
@@ -67,11 +67,103 @@ class Xmdp22SchemaPublicationFormatAdapter extends MetadataDataObjectAdapter {
 		$oaiDao = DAORegistry::getDAO('OAIDAO'); /* @var $oaiDao OAIDAO */
 		$publishedMonographDao = DAORegistry::getDAO('PublishedMonographDAO');
 		$monograph = $publishedMonographDao->getById($publicationFormat->getMonographId());
+		$series = $oaiDao->getSeries($monograph->getSeriesId()); /* @var $series Series */
 		$press = $oaiDao->getPress($monograph->getPressId());
 		$description = $this->instantiateMetadataDescription();
 
 		// Title
 		$this->_addLocalizedElements($description, 'dc:title', $monograph->getTitle(null));
+		
+		// Creator
+		$authors = $monograph->getAuthors();
+		foreach($authors as $author) {
+			$authorName = $author->getFullName(true);
+			$affiliation = $author->getLocalizedAffiliation();
+			if (!empty($affiliation)) {
+				$authorName .= '; ' . $affiliation;
+			}
+			$description->addStatement('dc:creator', $authorName);
+			unset($authorName);
+		}
+		
+		// Subject
+		$subjects = array_merge_recursive(
+				(array) $monograph->getDiscipline(null),
+				(array) $monograph->getSubject(null),
+				(array) $monograph->getSubjectClass(null));
+		$this->_addLocalizedElements($description, 'dc:subject', $subjects);
+		
+		// Publisher
+		$publisherInstitution = $press->getSetting('publisherInstitution');
+		if (!empty($publisherInstitution)) {
+			$publishers = array($press->getPrimaryLocale() => $publisherInstitution);
+		} else {
+			$publishers = $press->getName(null); // Default
+		}
+		$this->_addLocalizedElements($description, 'dc:publisher', $publishers);
+		
+		// Contributor
+		$contributors = $monograph->getSponsor(null);
+		if (is_array($contributors)) {
+			foreach ($contributors as $locale => $contributor) {
+				$contributors[$locale] = array_map('trim', explode(';', $contributor));
+			}
+			$this->_addLocalizedElements($description, 'dc:contributor', $contributors);
+		}
+		
+		// Type
+		$types = array_merge_recursive(
+				array(AppLocale::getLocale() => __('rt.metadata.pkp.dctype')),
+				(array) $monograph->getType(null)
+		);
+		$this->_addLocalizedElements($description, 'dc:type', $types);
+		
+		// Format
+		$onixCodelistItemDao = DAORegistry::getDAO('ONIXCodelistItemDAO');
+		$entryKeys = $onixCodelistItemDao->getCodes('List7'); // List7 is for object formats
+		if ($publicationFormat->getEntryKey()) {
+			$formatName = $entryKeys[$publicationFormat->getEntryKey()];
+			$description->addStatement('dc:format', $formatName);
+		}
+		
+		// Identifier: URL
+		if (is_a($monograph, 'PublishedMonograph')) {
+			$description->addStatement('dc:identifier', Request::url($press->getPath(), 'catalog', 'book', array($monograph->getId())));
+		}
+		
+		// Identifier: others
+		$identificationCodeFactory = $publicationFormat->getIdentificationCodes();
+		while ($identificationCode = $identificationCodeFactory->next()) {
+			$description->addStatement('dc:identifier', $identificationCode->getNameForONIXCode());
+		}
+		
+		// Source (press title and pages)
+		$sources = $press->getName(null);
+		$pages = $monograph->getPages();
+		if (!empty($pages)) $pages = '; ' . $pages;
+		foreach ($sources as $locale => $source) {
+			$sources[$locale] .= '; ';
+			$sources[$locale] .=  $pages;
+		}
+		$this->_addLocalizedElements($description, 'dc:source', $sources);
+		
+		// Language
+		
+		// Relation
+		
+		// Coverage
+		$coverage = array_merge_recursive(
+				(array) $monograph->getCoverageGeo(null),
+				(array) $monograph->getCoverageChron(null),
+				(array) $monograph->getCoverageSample(null));
+		$this->_addLocalizedElements($description, 'dc:coverage', $coverage);
+		
+		// Rights
+		$salesRightsFactory = $publicationFormat->getSalesRights();
+		while ($salesRight = $salesRightsFactory->next()) {
+			$description->addStatement('dc:rights', $salesRight->getNameForONIXCode());
+		}
+		
 
 		Hookregistry::call('Xmdp22SchemaPublicationFormatAdapter::extractMetadataFromDataObject', array(&$this, $monograph, $press, &$description));
 
